5458a5cfcdc0d995801f1c832d05e28bb801cc78
LingfengZhu <43455042+LingfengZhu@users.noreply.github.com>
2022-11-26 20:13:25 +0800

    Update run_sas_cts.py

--- run_sas_cts.py
+++ run_sas_cts.py
@@ -1,9 +1,13 @@
 from recbole.quick_start import run_recbole
 
 
-
+# 参数文件
 parameter_dict = {
    'neg_sampling': None,
 }
+
+# 配置文件
 config_file_list = ['sas_cts_2.yaml']
+
+# 训练代码
 run_recbole(model='SASCTS', dataset='ml-1m', config_file_list=config_file_list, config_dict=parameter_dict)
